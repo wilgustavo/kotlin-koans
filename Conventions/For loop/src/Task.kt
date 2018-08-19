45ac82ce636682eb4868c4ed6d8188ed118bea0c
wilgustavo <wilgustavo@gmail.com>
2018-08-19 20:46:51 -0300

    Conventions: For loop

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,18 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterador(start, end)
+}
+
+class DateIterador(start: MyDate, private val endInclusive: MyDate): Iterator<MyDate> {
+
+    private var current = start
+    override fun hasNext(): Boolean = current <= endInclusive
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
